ac00d5623ecc679377dd7f992af6f682369747ab
andimags <andimagsumbol@gmail.com>
2026-01-30 15:06:01 +0800

    feat: set up swagger, add global pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, AuthModule],
     controllers: [AppController],
-    providers: [AppService, PrismaService],
+    providers: [
+        AppService, 
+        PrismaService,
+    {
+        provide: APP_PIPE,
+        useClass: ValidationPipe,
+    },],
 })
 export class AppModule {}
