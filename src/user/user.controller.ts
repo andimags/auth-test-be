b8238e64ff017c8a62c6cbf2375ccbe053ac3cbb
andimags <andimagsumbol@gmail.com>
2026-01-30 15:53:28 +0800

    feat: add authGuard on findUser api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService){}
 
     @Get(':id')
+    @UseGuards(AuthGuard)
     async findUser(@Param('id') id: number){
         const user = await this.userService.user({
             id: id
